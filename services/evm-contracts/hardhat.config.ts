f0d2a214bee057da468ea59e0b4dfc012eddd2f3
Tiago Tavares <bigtavadev@gmail.com>
2023-05-10 16:30:39 +0100

    feat: adds governor contract

--- services/evm-contracts/hardhat.config.ts
+++ services/evm-contracts/hardhat.config.ts
@@ -92,10 +92,7 @@ const config: HardhatUserConfig = {
     solidity: {
         compilers: [
             {
-                version: "0.8.7",
-            },
-            {
-                version: "0.6.6",
+                version: "0.8.9",
             },
             {
                 version: "0.4.24",
