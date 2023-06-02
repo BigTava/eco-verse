c0eaf5f593918c46f54a618e53dccd60dc1e1a9e
Vitor Ribeiro <v99ribeiro@gmail.com>
2023-06-02 12:33:23 +0100

    powered by chainlink & eth

--- services/frontend/src/pages/Landing/Hero/index.tsx
+++ services/frontend/src/pages/Landing/Hero/index.tsx
@@ -9,14 +9,9 @@ import { DefaultButton as Button } from "components/Buttons/DefaultButton";
 import Container from "components/Container";
 
 // Assets
-import logoBbc from "assets/logos/bbc.svg";
-import logoCbs from "assets/logos/cbs.svg";
-import logoCnn from "assets/logos/cnn.svg";
-import logoFastCompany from "assets/logos/fast-company.svg";
-import logoForbes from "assets/logos/forbes.svg";
-import logoHuffpost from "assets/logos/huffpost.svg";
-import logoTechcrunch from "assets/logos/techcrunch.svg";
-import logoWired from "assets/logos/wired.svg";
+
+import logoChainlink from "assets/logos/Chainlink_Logo_Blue.svg";
+import logoEthereum from "assets/logos/ethereum-eth-logo-full-horizontal.svg";
 
 export function Hero() {
   const navigate = useNavigate();
@@ -56,21 +51,15 @@ export function Hero() {
           </div>
           <div className="relative -mt-4 lg:col-span-7 lg:mt-0 xl:col-span-6">
             <p className="text-center text-sm font-semibold text-gray-900 lg:text-left">
-              As featured in
+              Powered by
             </p>
             <ul
               role="list"
               className="mx-auto mt-8 flex max-w-xl flex-wrap justify-center gap-x-10 gap-y-8 lg:mx-0 lg:justify-start"
             >
               {[
-                ["Forbes", logoForbes],
-                ["TechCrunch", logoTechcrunch],
-                ["Wired", logoWired],
-                ["CNN", logoCnn, "hidden xl:block"],
-                ["BBC", logoBbc],
-                ["CBS", logoCbs],
-                ["Fast Company", logoFastCompany],
-                ["HuffPost", logoHuffpost, "hidden xl:block"],
+                ["Chainlink", logoChainlink],
+                ["Ethereum", logoEthereum],
               ].map(([name, logo, className]) => (
                 <li key={name} className={clsx("flex", className)}>
                   <img src={logo} alt={name} className="h-8" />
