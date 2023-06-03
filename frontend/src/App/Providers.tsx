90a6d8e118fa1ccc9b2f88c05b26bff1c0066e8f
Tiago Tavares <bigtavadev@gmail.com>
2023-06-03 17:59:52 +0100

    refactor: switches wagmi for moralis

--- frontend/src/App/Providers.tsx
+++ frontend/src/App/Providers.tsx
@@ -1,75 +1,22 @@
 // Core
 import React, { FC } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { MoralisProvider } from "react-moralis";
 
 // Contexts
 import MainProvider from "contexts/Main.context";
 import { Web3ContextProvider } from "contexts/Web3.context";
 import { UserProvider } from "contexts/User.context";
 
-// Wagmi
-import { WagmiConfig, createClient, configureChains } from "wagmi";
-import { sepolia, hardhat } from "@wagmi/core/chains";
-import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
-import { InjectedConnector } from "wagmi/connectors/injected";
-import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
-import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
-
 type ProvidersProps = {
   children: React.ReactNode;
 };
 
-const { chains, provider, webSocketProvider } = configureChains(
-  [sepolia, hardhat],
-  [
-    jsonRpcProvider({
-      rpc: () => ({
-        http: `${process.env.REACT_APP_SEPOLIA_RPC_URL}`,
-      }),
-    }),
-    jsonRpcProvider({
-      rpc: () => ({
-        http: `${process.env.REACT_APP_LOCALHOST_RPC_URL}`,
-      }),
-    }),
-  ]
-);
-
 const Providers: FC<ProvidersProps> = ({ children }) => {
   const queryClient = new QueryClient();
 
-  const client = createClient({
-    autoConnect: true,
-    connectors: [
-      new MetaMaskConnector({ chains }),
-      new CoinbaseWalletConnector({
-        chains,
-        options: {
-          appName: "wagmi",
-        },
-      }),
-      new WalletConnectConnector({
-        chains,
-        options: {
-          projectId: "72fe8cfe616d31f641b9199add6c17a3",
-          version: "2",
-        },
-      }),
-      new InjectedConnector({
-        chains,
-        options: {
-          name: "Injected",
-          shimDisconnect: true,
-        },
-      }),
-    ],
-    provider,
-    webSocketProvider,
-  });
-
   return (
-    <WagmiConfig client={client}>
+    <MoralisProvider initializeOnMount={false}>
       <QueryClientProvider client={queryClient}>
         <MainProvider>
           <Web3ContextProvider>
@@ -77,7 +24,7 @@ const Providers: FC<ProvidersProps> = ({ children }) => {
           </Web3ContextProvider>
         </MainProvider>
       </QueryClientProvider>
-    </WagmiConfig>
+    </MoralisProvider>
   );
 };
 
--- frontend/src/pages/CreateCommunity/index.tsx
+++ frontend/src/pages/CreateCommunity/index.tsx
@@ -1,8 +1,10 @@
 // Core
-import { useState } from "react";
+import { useState, ReactText } from "react";
 import { useUser } from "contexts/User.context";
 import { toast } from "react-toastify";
 import { useWeb3 } from "contexts/Web3.context";
+import { useWeb3Contract, useMoralis } from "react-moralis";
+import { ContractTransaction } from "ethers";
 
 // Components
 import { AuthLayout } from "components/Layouts/AuthLayout";
@@ -11,15 +13,6 @@ import { DefaultButton } from "components/Buttons/DefaultButton";
 // Form
 import GeneralInfo, { GeneralInfoValuesType } from "./GeneralInfo";
 /*eslint-disable*/
-// Wagmi
-import {
-  useContractWrite,
-  usePrepareContractWrite,
-  useWaitForTransaction,
-  useConnect,
-  useAccount,
-  useEnsName,
-} from "wagmi";
 
 // Utils
 import { communityFactoryAbi, contractAddresses } from "utils/constants";
@@ -31,6 +24,7 @@ import Navigation from "./Navigation";
 export default function CreateCommunity() {
   const { user } = useUser();
   const { web3 } = useWeb3();
+  const { chainId } = useMoralis();
 
   const [activeStep, setActiveStep] = useState(1);
   const [generalInfoValues, setGeneralInfoValues] =
@@ -40,21 +34,18 @@ export default function CreateCommunity() {
       longitude: null,
     });
 
+  /* Contract Calls */
   const {
-    config,
-    error: prepareError,
-    isError: isPrepareError,
-  } = usePrepareContractWrite({
-    address: contractAddresses["31337"]["communityFactory"] as `0x${string}`,
+    runContractFunction: createCommunity,
+    isLoading,
+    isFetching,
+  } = useWeb3Contract({
     abi: communityFactoryAbi,
+    contractAddress: contractAddresses["31337"][
+      "communityFactory"
+    ] as `0x${string}`, // specify the networkId
     functionName: "createCommunity",
-    args: Object.values(generalInfoValues),
-  });
-
-  const { data, write, error, isError } = useContractWrite(config);
-
-  const { isLoading, isSuccess } = useWaitForTransaction({
-    hash: data?.hash,
+    params: { ...Object.values(generalInfoValues) },
   });
 
   const canSave = () => {
@@ -83,17 +74,38 @@ export default function CreateCommunity() {
     }
 
     if (activeStep === 1) {
-      const args = { ...generalInfoValues };
-      try {
-        write?.();
-
-        if (isSuccess) {
-          return setActiveStep(activeStep + 1);
-        }
-      } catch (error) {
-        console.log(error);
-      }
+      const id = toast.loading("Please wait...", {
+        position: "top-right",
+        autoClose: 5000,
+        theme: "light",
+      });
+      await createCommunity({
+        onSuccess: (tx: any) => handleSuccess(id, tx),
+        onError: (error) => handleError(id, error),
+      });
     }
+  };
+
+  const handleSuccess = async (toastId: ReactText, tx: ContractTransaction) => {
+    await tx.wait();
+    toast.update(toastId, {
+      render: "Community created!",
+      type: toast.TYPE.SUCCESS,
+      position: "top-right",
+      isLoading: false,
+      autoClose: 1000,
+    });
+  };
+
+  const handleError = (toastId: ReactText, error: any) => {
+    console.log(error);
+    toast.update(toastId, {
+      render: "Error creating community!",
+      type: toast.TYPE.ERROR,
+      position: "top-right",
+      isLoading: false,
+      autoClose: 1000,
+    });
   };
 
   const getStep = (step: Number) => {
@@ -106,6 +118,12 @@ export default function CreateCommunity() {
 
         return {
           form: <GeneralInfo {...props} />,
+          buttonText: "Let's go!",
+        };
+
+      case 2:
+        return {
+          form: <span>{generalInfoValues.name} was successfully created!</span>,
           buttonText: "Let's go!",
         };
       default:
