613b5c03d5d9fdf813bb796c582f4bc44726a54a
vitorribeiro99 <v99ribeiro@gmail.com>
2023-06-09 00:59:23 +0100

    update: faqs

--- frontend/src/components/Faqs/index.tsx
+++ frontend/src/components/Faqs/index.tsx
@@ -3,53 +3,61 @@ import Container from "components/Container";
 const faqs = [
   [
     {
-      question: "What is an Energy Community?",
+      question:
+        "What is the EcoVerse project about and how does it relate to Energy Communities?",
       answer:
-        "Energy communities are open and voluntary and combine non-commercial aims with environmental and social community objectives.",
+        "EcoVerse is a web 3 project that provides a platform for the creation and management of Energy Communities. It was inspired by the need to combat climate change and empower local communities in managing their energy resources. This project facilitates collaborative decision-making and investment in renewable energy.",
     },
     {
-      question: "Isn’t this insider trading?",
+      question: "What benefits can I gain from creating an Energy Community?",
       answer:
-        "Yes exactly. But at scale! Historically you could only make insider trades with knowledge from your direct network. Pocket brings you insider trading tips from people you don’t even know.",
+        "By creating an Energy Community, you get the opportunity to take control of your energy resources and decisions. You can share energy within your community or with third-party providers, track your contributions to green energy production, and monitor CO2 emissions reduction.",
     },
     {
-      question: "But isn’t insider trading illegal?",
+      question:
+        "How does the autonomous governance system work in the Energy Communities?",
       answer:
-        "Here’s the thing: you’re the one doing the insider trading, not us. We’re just giving you the tips and some tools to make trades. We’re not doing anything wrong here.",
-    },
-  ],
-  [
-    {
-      question: "Do the people giving you tips realize what they are doing?",
-      answer:
-        "Again I would argue this isn’t really our responsibility. People make their own choices. If they don’t research the consequences that’s on them, not on us.",
-    },
-    {
-      question: "Where is Pocket based?",
-      answer:
-        "Let’s just say it’s not somewhere where the SEC is going to find us.",
-    },
-    {
-      question: "Is there any age limit to trading on Pocket?",
-      answer:
-        "For our free plan, the age limit is based on the minimum age to trade in your country of residence. Our VIP plan uses advanced transaction anonymization though, so you can use that plan even if you’re 9 years old. Or a dog.",
+        "The autonomous governance system allows community members to participate in decision-making processes. This could include deciding who joins the community, determining the amount of money to raise for renewable energy projects, and other decisions that could lead to significant community gains.",
     },
   ],
   [
     {
-      question: "How did you get this on the App Store?",
+      question:
+        "What is the process for onboarding members into my Energy Community?",
       answer:
-        "Honestly we were surprised too, but eventually we found out that the app reviewer found the app so compelling they approved it just so they could use it themselves.",
+        "The process for onboarding members into your Energy Community will depend on the governance decisions made by the community. The EcoVerse platform supports you throughout this process, ensuring a smooth and straightforward experience.",
     },
     {
-      question: "How do I explain the money I withdraw from Pocket to the IRS?",
+      question:
+        "Can you explain how the financing process works, especially the crowdlending campaigns for renewable energy projects?",
       answer:
-        "This feels like one-hundred percent a you problem. Pocket is not responsible in any way for your tax returns.",
+        "The financing process involves participating in crowdlending campaigns, where every participant can contribute towards renewable energy projects. This democratic funding model allows everyone in the community to have a stake in local energy infrastructure.",
     },
     {
-      question: "How do I become an insider?",
+      question:
+        "How does energy sharing work in the Energy Communities? Can this be extended to third-party providers?",
       answer:
-        "Contact us with some details about your industry and the type of access you have to apply for an insider account. Once approved, we’ll send you a guide on collecting insider information without being detected at work.",
+        "Energy sharing in the Energy Communities is facilitated by our platform. Community members can share energy amongst themselves or even with third-party providers, enhancing the community's sustainability and energy independence.",
+    },
+  ],
+  [
+    {
+      question:
+        "What metrics are available through the sustainability tracking feature? How can I understand the impact of my investments?",
+      answer:
+        "The sustainability tracking feature lets you monitor your contributions to green energy production and your role in reducing CO2 emissions. It offers clear metrics and visuals that help you understand the tangible impact of your investments on the environment.",
+    },
+    {
+      question:
+        "How does being a part of an Energy Community contribute to community growth and sustainable energy development?",
+      answer:
+        "Being part of an Energy Community allows you to witness firsthand how your initial investment catalyzes sustainable energy development. It sets a new standard for investors and contributes to the growth of your community by creating local jobs and reducing reliance on external energy suppliers.",
+    },
+    {
+      question:
+        "What are some ways in which the platform helps users in decision-making processes for buying and selling energy?",
+      answer:
+        "Leveraging insights from our network of industry insiders, our platform provides information on when to buy energy to maximize profit and when to sell to avoid losses. This market intelligence empowers Energy Communities to make informed decisions that align with their goals and financial strategies.",
     },
   ],
 ];
@@ -70,6 +78,7 @@ export function Faqs() {
             Frequently asked questions
           </h2>
         </div>
+        <div className="divider"></div>
         <ul
           role="list"
           className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-8 sm:mt-20 lg:max-w-none lg:grid-cols-3"
@@ -78,7 +87,10 @@ export function Faqs() {
             <li key={columnIndex}>
               <ul role="list" className="space-y-10">
                 {column.map((faq, faqIndex) => (
-                  <li key={faqIndex}>
+                  <li
+                    key={faqIndex}
+                    className="rounded-2xl border border-gray-300 p-8 hover:animate-fade-in hover:border-gray-600"
+                  >
                     <h3 className="text-lg font-semibold leading-6 text-gray-900">
                       {faq.question}
                     </h3>
