77118b8fd5eda7ee63b7b8c2932545677c3cd951
vitorribeiro99 <v99ribeiro@gmail.com>
2023-06-09 01:11:03 +0100

    add:footer

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,65 @@
+import { DefaultButton } from "components/Buttons/DefaultButton";
+import Container from "components/Container";
+import { TextField } from "components/Fields";
+import NavLinks from "components/Header/NavLinks";
+import logo from "assets/images/EcoVerse-logo.png";
+
+function QrCodeBorder(props) {
+  return (
+    <svg viewBox="0 0 96 96" fill="none" aria-hidden="true" {...props}>
+      <path
+        d="M1 17V9a8 8 0 0 1 8-8h8M95 17V9a8 8 0 0 0-8-8h-8M1 79v8a8 8 0 0 0 8 8h8M95 79v8a8 8 0 0 1-8 8h-8"
+        strokeWidth="2"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
+
+export function Footer() {
+  return (
+    <footer className="border-t border-gray-200">
+      <Container>
+        <div className="flex flex-col items-start justify-between gap-y-12 pb-6 pt-16 lg:flex-row lg:items-center lg:py-16">
+          <div>
+            <div className="flex items-center text-gray-900">
+              <img src={logo} alt="logo" width="160px"></img>
+              <div className="ml-4">
+                <p className="text-base font-semibold">Ecoverse</p>
+                <p className="mt-1 text-sm">
+                  Your Energy Community starts here!
+                </p>
+              </div>
+            </div>
+            <nav className="mt-11 flex gap-8">
+              <NavLinks />
+            </nav>
+          </div>
+          <div className="group relative -mx-4 flex items-center self-stretch p-4 transition-colors hover:bg-gray-100 sm:self-auto sm:rounded-2xl lg:mx-0 lg:self-auto lg:p-6">
+            <div className="relative flex h-24 w-24 flex-none items-center justify-center"></div>
+            <div className="ml-8 lg:w-64"></div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center border-t border-gray-200 pb-12 pt-8 md:flex-row-reverse md:justify-between md:pt-6">
+          <form className="flex w-full justify-center md:w-auto">
+            <TextField
+              type="email"
+              aria-label="Email address"
+              placeholder="Email address"
+              autoComplete="email"
+              required
+              className="w-60 min-w-0 shrink"
+            />
+            <DefaultButton variant="solid" className="ml-4 flex-none">
+              <span className="hidden">Join our newsletter</span>
+              <span className="lg:hidden">Join newsletter</span>
+            </DefaultButton>
+          </form>
+          <p className="mt-6 text-sm text-gray-500 md:mt-0">
+            &copy; Copyright {new Date().getFullYear()}. All rights reserved.
+          </p>
+        </div>
+      </Container>
+    </footer>
+  );
+}
